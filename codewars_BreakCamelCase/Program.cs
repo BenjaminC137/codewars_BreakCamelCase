feb513268c6a9ded7397146373d6f7bc1a4cff8d
benjaminc137 <benjamin.salasek@icloud.com>
2018-09-12 14:19:52 -0500

    initial

---
+++ codewars_BreakCamelCase/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace codewars_BreakCamelCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(BreakCamelCase("helloWorld!"));
+        }
+        
+         public static string BreakCamelCase(string str)
+          {
+          //  foreach(char letter in str){
+            
+          //}
+            
+            str.Where(x => x.IsUpper);            
+            
+            return "balls";
+          }
+    }
+}
+//Complete the solution so that the function will break up camel casing, using a space between words.
+//Kata.BreakCamelCase("camelCasing") => "camel Casing"
